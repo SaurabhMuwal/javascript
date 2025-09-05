895134cfead65a64bd0c25362f9264b70fff4048
Saurabh Muwal <saurabhmuwal009@gmail.com>
2025-09-05 13:46:17 +0000

    datatypes summary

--- basics/05_datatypes-summary.js
+++ basics/05_datatypes-summary.js
@@ -32,3 +32,27 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+// ++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non - Primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
